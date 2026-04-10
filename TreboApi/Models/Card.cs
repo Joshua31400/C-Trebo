f2081c354aed2a0c7e1ccf16c7908d2b1ffd1429
bjoshua <joshua.budgen@ynov.com>
2026-04-10 11:32:43 +0200

    Add card, comment and user models

--- TreboApi/Models/Card.cs
+++ TreboApi/Models/Card.cs
@@ -2,5 +2,12 @@
 
 public class Card
 {
-    
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public list<member> Members { get; set; }
+    public list<Comment> Comments { get; set; }
+    public int ColumnId { get; set; }
+    public Column Column { get; set; }
 }
--- TreboApi/Models/Comment.cs
+++ TreboApi/Models/Comment.cs
@@ -2,5 +2,11 @@
 
 public class Comment
 {
-    
+    public int Id { get; set; }
+    public string Content { get; set; }
+    public int CreatorId  { get; set; }
+    public User Creator { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public int CardId { get; set; }
+    public Card Card { get; set; }
 }
