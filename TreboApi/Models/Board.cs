0af0b0160ccbaf22b7e44a34dab4078db8cf63f3
Pedro Martins <pedrohenrique.costamartins@ynov.com>
2026-04-10 11:12:44 +0200

    add models - add base board and column models

---
+++ TreboApi/Models/Board.cs
@@ -0,0 +1,11 @@
+﻿namespace DefaultNamespace;
+
+public class Board
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public List<Column> Columns { get; set; }
+    public List<Column> ArchivedColumns { get; set; } 
+    public List<User> Members { get; set; }
+}
---
+++ TreboApi/Models/Column.cs
@@ -0,0 +1,11 @@
+﻿namespace DefaultNamespace;
+
+public class Column
+{
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public List<Card> Cards { get; set; }
+        public List<Card> ArchivedCards { get; set; }
+        public int BoardId { get; set; }
+        public Board Board { get; set; }
+}
