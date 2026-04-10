a5fc7f6d7e783ef2e350ee27a9eb99268224295c
Pedro Martins <pedrohenrique.costamartins@ynov.com>
2026-04-10 12:00:06 +0200

    add dependencies - add db sets to db context

--- TreboApi/AppDbContext.cs
+++ TreboApi/AppDbContext.cs
@@ -4,6 +4,12 @@ namespace treboapi;
 
 public class AppDbContext : DbContext
 {
+    public DbSet<Board> Boards { get; set; }
+    public DbSet<Column> Columns { get; set; }
+    public DbSet<Card> Cards { get; set; }
+    public DbSet<Label> Labels { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Comment> Comments { get; set; }
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
     }
--- TreboApi/Program.cs
+++ TreboApi/Program.cs
@@ -1,6 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite("Data Source=trebo.db"));
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
+
+app.UseCors();
 
 app.Run();
