21c8f3bd4a5c6719fa6721a129b04499dbdde44a
Pedro Martins <pedrohenrique.costamartins@ynov.com>
2026-04-10 11:47:43 +0200

    add label model - add creator and labels to board model

--- TreboApi/Models/Board.cs
+++ TreboApi/Models/Board.cs
@@ -8,4 +8,7 @@ public class Board
     public List<Column> Columns { get; set; }
     public List<Column> ArchivedColumns { get; set; } 
     public List<User> Members { get; set; }
+    public List<Label> UniqueLabels { get; set; }
+    public int CreatorId { get; set; }
+    public User Creator { get; set; }
 }
---
+++ TreboApi/Models/Label.cs
@@ -0,0 +1,12 @@
+﻿namespace DefaultNamespace;
+
+public class Label
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Color { get; set; }
+    public bool IsDefault { get; set; }
+    public int? BoardId { get; set; }
+    public Board Board { get; set; }
+    public List<Card> Cards { get; set; }
+}
