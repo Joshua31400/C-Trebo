0b4bebaf1783acf8f262cb80263183d6917a7cce
Pedro Martins <pedrohenrique.costamartins@ynov.com>
2026-04-10 09:18:55 +0200

    add dotnet web and api

---
+++ TreboWeb/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TreboWeb.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
